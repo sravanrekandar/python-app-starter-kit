280b8db9905046792af09cc52145b27b3a6b8248
Sravan Kumar <sravanrekandar@gmail.com>
2021-04-24 15:14:59 +0530

    remove unused files

--- start.bat
+++ start.bat
@@ -3,4 +3,3 @@ call .venv\\Scripts\\activate
 call python -m pip install --upgrade pip
 call python3 -m pip install pipenv
 call python3 -m pipenv install --dev --pre
-call python3 vscode-env-setup.py
